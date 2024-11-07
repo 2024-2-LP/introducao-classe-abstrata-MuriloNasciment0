b78416bd1e2be90f5ff6022ca7915afeaa1a521c
MuriloNasciment0 <murilo.barros@sptech.school>
2024-11-07 18:05:59 -0300

    Subindo exercicio completo

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,45 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {
+        this.figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        double soma = 0.0;
+        for (Figura figura : figuras) {
+            soma += figura.calcularArea();
+        }
+        return soma;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> figurasMaiorQue20 = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20) {
+                figurasMaiorQue20.add(figura);
+            }
+        }
+        return figurasMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura> quadrados = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado) {
+                quadrados.add(figura);
+            }
+        }
+        return quadrados;
+    }
 }
+
--- src/main/java/school/sptech/Triangulo.java
+++ src/main/java/school/sptech/Triangulo.java
@@ -1,4 +1,28 @@
 package school.sptech;
 
-public class Triangulo {
+public class Triangulo extends Figura{
+    private Double base;
+    private Double altura;
+
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public Double calcularArea() {
+        return (base * altura) / 2;
+    }
 }
